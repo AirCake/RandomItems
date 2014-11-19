bd6bef25bef86593a25245e45960f7f2a4c2b629
AirCake <nrgstar@Ido-Cohens-Mac-mini.local>
2014-11-19 15:47:15 +0000

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Ido Cohen on 19/11/2014.
+//  Copyright (c) 2014 BNR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
